bb1dec594a2571c72d76f50e9a72a541d80714b7
RazanBoodai <51971623+RazanBoodai@users.noreply.github.com>
2019-07-09 21:45:08 +0300

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,7 @@
+name = input ("What is your name? ")
+age = input ("How old are you? ")
+
+numberofyearsto100 = 100 -int(age)
+yearof100age = 2018 + numberofyearsto100
+
+print("Your name is " + name " and will be 100s old in" + str(yearOf100Age))
